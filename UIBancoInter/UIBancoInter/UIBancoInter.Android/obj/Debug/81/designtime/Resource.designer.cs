5e932fd6a2f4c89a1a0e9a8b7dc51de2de00c687
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-02-13 17:02:56 -0300

    Pagina Login

--- UIBancoInter/UIBancoInter/UIBancoInter.Android/obj/Debug/81/designtime/Resource.designer.cs
+++ UIBancoInter/UIBancoInter/UIBancoInter.Android/obj/Debug/81/designtime/Resource.designer.cs
@@ -4852,6 +4852,9 @@ namespace UIBancoInter.Droid
 			// aapt resource value: 0x7F090104
 			public const int ic_vol_type_tv_light = 2131296516;
 			
+			// aapt resource value: 0x7F010002
+			public const int img_avatar = 2130771970;
+			
 			// aapt resource value: 0x7F090105
 			public const int mr_button_connected_dark = 2131296517;
 			
@@ -4951,8 +4954,8 @@ namespace UIBancoInter.Droid
 			// aapt resource value: 0x7F09000C
 			public const int notify_panel_notification_icon_bg = 2131296268;
 			
-			// aapt resource value: 0x7F010002
-			public const int outdoors = 2130771970;
+			// aapt resource value: 0x7F010003
+			public const int outdoors = 2130771971;
 			
 			// aapt resource value: 0x7F090069
 			public const int tooltip_frame_dark = 2131296361;
