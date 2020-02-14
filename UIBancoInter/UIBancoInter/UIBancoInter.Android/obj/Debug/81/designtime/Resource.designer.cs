e1dcc5f3acd0e45215f41ea9d7e5f583f9314515
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-02-14 10:08:04 -0300

    Tela Login

--- UIBancoInter/UIBancoInter/UIBancoInter.Android/obj/Debug/81/designtime/Resource.designer.cs
+++ UIBancoInter/UIBancoInter/UIBancoInter.Android/obj/Debug/81/designtime/Resource.designer.cs
@@ -4954,9 +4954,6 @@ namespace UIBancoInter.Droid
 			// aapt resource value: 0x7F09000C
 			public const int notify_panel_notification_icon_bg = 2131296268;
 			
-			// aapt resource value: 0x7F010003
-			public const int outdoors = 2130771971;
-			
 			// aapt resource value: 0x7F090069
 			public const int tooltip_frame_dark = 2131296361;
 			
